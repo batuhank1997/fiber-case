86dc60cb5d94afa1928f959104d5b4d493958bc1
Batuhan <batuhank1997@hotmail.com>
2023-12-06 14:35:07 +0300

    event system manager added

---
+++ Assets/_Dev/Game/Scripts/EventSystem/EventId.cs
@@ -0,0 +1,14 @@
+namespace _Dev.Game.Scripts.EventSystem
+{
+    public enum EventId
+    {
+        #region Game
+        on_game_initialized = 0,
+        #endregion
+
+        #region InGame
+        on_wave_started = 10,
+        on_wave_ended = 11,
+        #endregion
+    }
+}
