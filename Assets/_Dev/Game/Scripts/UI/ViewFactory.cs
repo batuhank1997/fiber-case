0356ddbb49d9065c9f8b4b2a75ae2ddc2a9b4ce9
Batuhan <batuhank1997@hotmail.com>
2023-12-06 16:09:59 +0300

    singleton, UI management

---
+++ Assets/_Dev/Game/Scripts/UI/ViewFactory.cs
@@ -0,0 +1,28 @@
+﻿using _Dev.Game.Scripts.UI.Views.Base;
+using _Dev.Utilities.Singleton;
+
+namespace _Dev.Game.Scripts.UI
+{
+    public class ViewFactory : Singleton<ViewFactory>
+    {
+        private static View _existingUIController;
+
+        public static View Create<T>() where T : View
+        {
+            if (IsExists<T>())
+                return _existingUIController;
+            
+            var viewPrefab = ViewContainer.Instance.GetViewPrefab<T>();
+            var instance = Instantiate(viewPrefab, Instance.transform);
+
+            return instance;
+        }
+        
+        //todo: refactor this
+        public static bool IsExists<T>() where T : View
+        {
+            _existingUIController = FindObjectOfType<T>();
+            return _existingUIController;
+        }
+    }
+}
